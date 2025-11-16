466d445fbfaaae55102b848532fbdc70513302ed
spencermuparutsa-create <spencer.muparutsa@gmail.com>
2025-11-16 09:59:01 +0100

    Add files via upload

--- satellites.py
+++ satellites.py
@@ -4,16 +4,33 @@ import random
 WIDTH = 700
 HEIGHT = 600
 
-satelite = Actor("satellite")
+sate = []
+next = 0
+line = []
 
-satelite.x = random.randint(0,700)
-satelite.y = random.randint(0,600)
+for i in range(10):
+    satelite = Actor("satellite")
+    satelite.x = random.randint(0,700)
+    satelite.y = random.randint(0,600)
+    sate.append(satelite)
 
 
 def draw():
     screen.blit("background", (0,0))
-    satelite.draw()
+    numb = 1
+    for i in sate:
+        i.draw()
+        screen.draw.text(str(numb),(i.x, i.y - 25))
+        numb = numb + 1
+    for e in line:
+        screen.draw.line(e[0],e[1],"white")
 
+def on_mouse_down(pos):
+    global next
+    if sate [next].collidepoint(pos):
+        if next >0:
+            line.append((sate[next - 1].pos,sate[next].pos))
+        next = next + 1
 
 
 
