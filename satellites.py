7c2ab5609e9350c0841de8c6557afb5b2a2a262a
spencermuparutsa-create <spencer.muparutsa@gmail.com>
2025-11-23 09:35:12 +0100

    Add files via upload

--- satellites.py
+++ satellites.py
@@ -1,5 +1,6 @@
 import pgzrun
 import random
+from time import time
 
 WIDTH = 700
 HEIGHT = 600
@@ -7,6 +8,8 @@ HEIGHT = 600
 sate = []
 next = 0
 line = []
+
+start = time()
 
 for i in range(10):
     satelite = Actor("satellite")
@@ -16,6 +19,7 @@ for i in range(10):
 
 
 def draw():
+    global total
     screen.blit("background", (0,0))
     numb = 1
     for i in sate:
@@ -24,13 +28,25 @@ def draw():
         numb = numb + 1
     for e in line:
         screen.draw.line(e[0],e[1],"white")
+    if next <10:
+        total = time() - start
+        screen.draw.text(str(round (total, 2)), (50,50))
+    else:
+        screen.draw.text(str(round (total, 2)), (50,50))
 
 def on_mouse_down(pos):
-    global next
+    global next, line
     if sate [next].collidepoint(pos):
         if next >0:
             line.append((sate[next - 1].pos,sate[next].pos))
         next = next + 1
+
+    else:
+        line = []
+        next = 0
+
+def update():
+    pass
 
 
 
